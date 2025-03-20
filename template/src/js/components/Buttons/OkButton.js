f5b997228e36a113ecd30ef1dd27607a92916174
Oleksandr <zadorozhniy3@icloud.com>
2025-03-20 15:55:45 +0200

    upgrade

--- template/src/js/components/Buttons/OkButton.js
+++ template/src/js/components/Buttons/OkButton.js
@@ -2,9 +2,10 @@ import {span} from "aprog";
 import {closeThisModal} from "../../helpers/modal.js";
 import {translate} from "../../helpers/translator.js";
 
-export const OkButton = () => {
+export const OkButton = ({ className= "", style = {} } = {}) => {
     return span({
-        class: "btn",
+        class: `btn ${className}`,
+        style: style,
         onClick: (event) => closeThisModal(event.target),
         value: translate("ok")
     })
--- template/src/js/pages/Home.js
+++ template/src/js/pages/Home.js
@@ -1,13 +1,17 @@
-import {div, img,  button} from "aprog";
+import {div, img,  button, pre} from "aprog";
 import {translate} from "@/js/helpers/translator.js";
 import {Modal} from "@/js/components/Modal/Modal.js";
 import {OkButton} from "@/js/components/Buttons/OkButton.js";
+import {getVersion} from "@/js/helpers/helper.js";
 
-export const Home = () => {
+export const Home = (props = {}) => {
+    console.log(props);
+
     return div({
-        class: "w-full h-full p-5 flex flex-col items-center justify-center gap-5 text-base",
+        class: "w-full h-screen p-2 flex flex-col items-center justify-center gap-5 text-base bg-cloud",
         value: [
             img({
+                class: "logo",
                 src: "/logo.svg",
                 alt: "logo.svg"
             }),
@@ -15,12 +19,41 @@ export const Home = () => {
                 class: "text-3xl",
                 value: translate("web_aprog")
             }),
+            div({
+                class: "text-xxs",
+                value: translate("version", { version: getVersion() })
+            }),
+            div({ value: translate("copyright") }),
             button({
-                class: "btn",
+                class: "btn text-white bg-default",
                 onClick: () => Modal({
                     title: translate("modal_title"),
-                    body: translate("hello_world"),
-                    btns: OkButton()
+                    body: div({
+                        class: "h-full flex flex-col items-center gap-2",
+                        value: [
+                            div({
+                                class: "text-5xl text-gold",
+                                value: "tryzub"
+                            }),
+                            div({
+                                class: "text-3xl",
+                                value: translate("thank_you")
+                            }),
+                            pre({
+                                class: "text-center whitespace-pre-wrap",
+                                value: translate("about_web_aprog")
+                            })
+                        ]
+                    }),
+                    btns: OkButton({
+                        className: "text-white bg-default"
+                    }),
+                    styles: {
+                        maxWidth: "80%",
+                        maxHeight: "80vh",
+                        backgroundColor: "#61b8f8",
+                        overflowY: "scroll"
+                    }
                 }),
                 value: translate("click_me")
             })
